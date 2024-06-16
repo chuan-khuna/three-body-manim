fd78e4cb327b3ec10437a6e6979b917eb818f2b6
ALTR <chuan.khuna@gmail.com>
2024-06-16 17:13:30 +0700

    move deprecated code

--- animation.py
+++ animation.py
@@ -17,15 +17,15 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def initialise_stars(configs, weights, G=1):
+def initialise_stars(configs, masses, G=1):
     # https://observablehq.com/@rreusser/periodic-planar-three-body-orbits
     stars = {}
-    num_stars = len(weights)
+    num_stars = len(masses)
     for i in range(num_stars):
         idx = 4 * i
         position = [configs[idx], configs[idx + 1]]
         velocity = [configs[idx + 2], configs[idx + 3]]
-        weight = weights[i]
+        weight = masses[i]
         star = Body(weight, np.array(position), np.array(velocity), f'star_{i}')
         star.G = G
         stars[f'star_{i}'] = star
@@ -39,50 +39,30 @@ class NBodyAnimation(Scene):
 
     def __init__(self):
         super().__init__()
-        self.stars = initialise_stars(
-            configs=[
-                -0.337076702,
-                0,
-                0,
-                0.9174260238,
-                2.1164029743,
-                0,
-                0,
-                -0.0922665014,
-                -1.7793262723,
-                0,
-                0,
-                -0.8251595224,
-            ],
-            weights=[1, 1, 1],
-            G=1,
-        )
 
-        self.stars = initialise_stars(
-            configs=[
-                -1,
-                0,
-                0.186238,
-                0.578714,
-                1,
-                0,
-                0.186238,
-                0.578714,
-                0,
-                0,
-                -0.372476,
-                -1.157428,
-            ],
-            weights=[1, 1, 1],
-            G=1,
-        )
+        position_velocity = [
+            -0.337076702,
+            0,
+            0,
+            0.9174260238,
+            2.1164029743,
+            0,
+            0,
+            -0.0922665014,
+            -1.7793262723,
+            0,
+            0,
+            -0.8251595224,
+        ]
+        masses = [1, 1, 1]
 
-        self.dt = 1e-4
-        self.num_steps = 500_000
+        self.stars = initialise_stars(configs=position_velocity, masses=masses, G=1)
+
+        self.dt = 1e-3
+        self.num_steps = 1e5
+        self.df = self.compute()
 
         self.star_radius = 0.025
-
-        self.df = self.compute()
 
     def compute(self) -> pd.DataFrame:
         star_positions = {}
@@ -92,7 +72,7 @@ class NBodyAnimation(Scene):
         for k, star in stars.items():
             star_positions[k] = [star.position]
 
-        for i in tqdm(
+        for step in tqdm(
             range(0, int(self.num_steps)),
             desc='Computing trajectories',
             ncols=100,
@@ -116,11 +96,16 @@ class NBodyAnimation(Scene):
         df = pd.concat(dfs).reset_index(drop=True)
 
         fig = plt.figure(figsize=(6, 6))
-        sns.scatterplot(data=df, x='x', y='y', hue='star', s=4)
+        sns.scatterplot(data=df, x='x', y='y', hue='star', s=2)
+        ax = plt.gca()
+        ax.legend().set_visible(False)
         plt.savefig('nbody.png')
         return df
 
-    def sampling_df(self, df, num_samples=1000):
+    def _sample_df(self, df, num_samples=1000):
+        """To reduce the number of points in the plot
+        sample data from the calculated trajectories
+        """
         star_names = df['star'].unique()
         sampled_dfs = []
         for star_name in star_names:
@@ -130,92 +115,72 @@ class NBodyAnimation(Scene):
             sampled_dfs.append(sampled_df)
         return pd.concat(sampled_dfs).reset_index(drop=True)
 
-    def construct(self):
-        num_samples = 500
-        move_run_time = 0.001
+    def create_star(self, x, y, colour):
+        print(f"Creating star at {x}, {y}")
+        star_circle = Circle(radius=self.star_radius, color=colour)
+        star_circle.move_to([x, y, 0])
+        return star_circle
 
-        df = self.sampling_df(self.df, num_samples=num_samples)
+    def _get_star_df_dict(self, df):
+        star_dict = {}
+        for star_name in df['star'].unique():
+            star_df = df[df['star'] == star_name].reset_index(drop=True)
+            star_dict[star_name] = star_df
+        return star_dict
+
+    def construct(self):
+
+        num_samples = 500
+        frame_time = 0.001
+
+        # print(f"{num_samples}, {frame_time}s = {num_samples * frame_time} s")
+
+        df = self._sample_df(self.df, num_samples=num_samples)
+        df_by_star = self._get_star_df_dict(df)
 
         star_names = df['star'].unique()
 
         star_objs = {}
-        star_trace_objs = {}
-        star_df_dict = {
-            star_name: df[df['star'] == star_name].reset_index(drop=True)
-            for star_name in star_names
-        }
-        star_full_df_dict = {
-            star_name: df[df['star'] == star_name].reset_index(drop=True)
-            for star_name in star_names
-        }
+        star_trackers: dict[str, list['ValueTracker']] = {}
+        star_traces = {}
 
         # intialise stars
         for i, star_name in enumerate(star_names):
-            star_df = df[df['star'] == star_name].reset_index(drop=True)
-            star = Circle(radius=self.star_radius, color=colours[i], fill_opacity=1).shift(
-                [star_df['x'].iloc[0], star_df['y'].iloc[0], 0]
+            star_df = df_by_star[star_name]
+            star = self.create_star(star_df['x'].iloc[0], star_df['y'].iloc[0], colours[i])
+
+            x_tracker = ValueTracker(df_by_star[star_name]['x'].iloc[0])
+            y_tracker = ValueTracker(df_by_star[star_name]['y'].iloc[0])
+            star_trackers[star_name] = [x_tracker, y_tracker]
+            star.add_updater(
+                lambda starobj, starname=star_name: starobj.move_to(
+                    [
+                        star_trackers[starname][0].get_value(),
+                        star_trackers[starname][1].get_value(),
+                        0,
+                    ]
+                )
             )
+
             star_objs[star_name] = star
 
-        for star_obj in star_objs.values():
-            self.add(star_obj)
-
-        # self.play(*[Write(star_obj) for star_obj in star_objs.values()])
+        # initialise the starting positions
+        self.add(*[star for star in star_objs.values()])
         self.wait(0.25)
 
-        for i in range(num_samples):
-            new_star_objs = {}
-            new_star_trace_objs = {}
-            transformations = []
+        for step in range(num_samples):
+            args = []
             for star_name in star_names:
-                # current star object
-                star = star_objs[star_name]
+                # update star positions
+                x_tracker, y_tracker = star_trackers[star_name]
+                x = df_by_star[star_name]['x'].iloc[step]
+                y = df_by_star[star_name]['y'].iloc[step]
+                args.append((x_tracker, x))
+                args.append((y_tracker, y))
 
-                # trace
-                max_trace_length = 10
-                start_idx = max(0, i - max_trace_length)
-                if star_name not in star_trace_objs.keys():
-                    star_trace_objs[star_name] = self.create_trace(
-                        star,
-                        star_df_dict[star_name]['x'].iloc[start_idx:i],
-                        star_df_dict[star_name]['y'].iloc[start_idx:i],
-                    )
-                    self.add(star_trace_objs[star_name])
-                else:
-                    new_trace = self.create_trace(
-                        star,
-                        star_df_dict[star_name]['x'].iloc[start_idx:i],
-                        star_df_dict[star_name]['y'].iloc[start_idx:i],
-                    )
-                    transformations.append(
-                        ReplacementTransform(star_trace_objs[star_name], new_trace)
-                    )
+            # create animations for each star
+            star_animations = [tracker.animate.set_value(val) for tracker, val in args]
 
-                # update position
-                newx = star_df_dict[star_name]['x'].iloc[i]
-                newy = star_df_dict[star_name]['y'].iloc[i]
-                moved_star = self.move_star(star, newx, newy)
+            # create animation for traces
 
-                # prepare for next iteration
-                new_star_objs[star_name] = moved_star
-                transformations.append(ReplacementTransform(star, moved_star))
-
-            self.play(*transformations, run_time=move_run_time)
-            star_objs = new_star_objs
-            star_trace_objs = new_star_trace_objs
-
-    def move_star(self, star_obj, x, y):
-        return star_obj.copy().move_to([x, y, 0])
-
-    def create_trace(self, star_obj, xs, ys):
-        color = star_obj.get_color()
-        xs = np.array(xs)
-        ys = np.array(ys)
-        dots = [Dot([xs[i], ys[i], 0], color=color, radius=0.005) for i in range(len(xs))]
-        arcs = [
-            Line(color=color, stroke_width=0.2, stroke_opacity=0.25).put_start_and_end_on(
-                dots[i].get_center(), dots[i + 1].get_center()
-            )
-            for i in range(len(dots) - 1)
-        ]
-        return VGroup(*arcs)
+            self.play(*star_animations, run_time=frame_time)
---
+++ depreacted/animation.py
@@ -0,0 +1,221 @@
+from manim import *
+
+config["frame_size"] = (2560, 1080)
+config["frame_rate"] = 60
+config["background_color"] = BLACK
+
+import numpy as np
+
+from utils.vis_utils import *
+
+from libs.body import Body
+from libs.constants import *
+
+from copy import deepcopy
+import pandas as pd
+
+from tqdm import tqdm
+
+
+def initialise_stars(configs, weights, G=1):
+    # https://observablehq.com/@rreusser/periodic-planar-three-body-orbits
+    stars = {}
+    num_stars = len(weights)
+    for i in range(num_stars):
+        idx = 4 * i
+        position = [configs[idx], configs[idx + 1]]
+        velocity = [configs[idx + 2], configs[idx + 3]]
+        weight = weights[i]
+        star = Body(weight, np.array(position), np.array(velocity), f'star_{i}')
+        star.G = G
+        stars[f'star_{i}'] = star
+    return stars
+
+
+colours = [RED, GREEN, BLUE, YELLOW, PURPLE, ORANGE, TEAL, PINK, GREY]
+
+
+class NBodyAnimation(Scene):
+
+    def __init__(self):
+        super().__init__()
+        self.stars = initialise_stars(
+            configs=[
+                -0.337076702,
+                0,
+                0,
+                0.9174260238,
+                2.1164029743,
+                0,
+                0,
+                -0.0922665014,
+                -1.7793262723,
+                0,
+                0,
+                -0.8251595224,
+            ],
+            weights=[1, 1, 1],
+            G=1,
+        )
+
+        self.stars = initialise_stars(
+            configs=[
+                -1,
+                0,
+                0.186238,
+                0.578714,
+                1,
+                0,
+                0.186238,
+                0.578714,
+                0,
+                0,
+                -0.372476,
+                -1.157428,
+            ],
+            weights=[1, 1, 1],
+            G=1,
+        )
+
+        self.dt = 1e-4
+        self.num_steps = 500_000
+
+        self.star_radius = 0.025
+
+        self.df = self.compute()
+
+    def compute(self) -> pd.DataFrame:
+        star_positions = {}
+
+        stars = deepcopy(self.stars)
+
+        for k, star in stars.items():
+            star_positions[k] = [star.position]
+
+        for i in tqdm(
+            range(0, int(self.num_steps)),
+            desc='Computing trajectories',
+            ncols=100,
+            total=int(self.num_steps),
+        ):
+            # current states
+            backup_stars = deepcopy(stars)
+
+            for k, star in stars.items():
+                other_stars = [other for k, other in backup_stars.items()]
+                updated_star = star.update(other_stars, self.dt)
+                stars[k] = updated_star
+                star_positions[k].append(updated_star.position)
+
+        dfs = []
+        for k, v in star_positions.items():
+            df = pd.DataFrame(v, columns=['x', 'y'])
+            df['star'] = k
+            dfs.append(df)
+
+        df = pd.concat(dfs).reset_index(drop=True)
+
+        fig = plt.figure(figsize=(6, 6))
+        sns.scatterplot(data=df, x='x', y='y', hue='star', s=4)
+        plt.savefig('nbody.png')
+        return df
+
+    def sampling_df(self, df, num_samples=1000):
+        star_names = df['star'].unique()
+        sampled_dfs = []
+        for star_name in star_names:
+            star_df = df[df['star'] == star_name].reset_index(drop=True)
+            step_size = len(star_df) // num_samples
+            sampled_df = star_df.iloc[::step_size]
+            sampled_dfs.append(sampled_df)
+        return pd.concat(sampled_dfs).reset_index(drop=True)
+
+    def construct(self):
+        num_samples = 500
+        move_run_time = 0.001
+
+        df = self.sampling_df(self.df, num_samples=num_samples)
+
+        star_names = df['star'].unique()
+
+        star_objs = {}
+        star_trace_objs = {}
+        star_df_dict = {
+            star_name: df[df['star'] == star_name].reset_index(drop=True)
+            for star_name in star_names
+        }
+        star_full_df_dict = {
+            star_name: df[df['star'] == star_name].reset_index(drop=True)
+            for star_name in star_names
+        }
+
+        # intialise stars
+        for i, star_name in enumerate(star_names):
+            star_df = df[df['star'] == star_name].reset_index(drop=True)
+            star = Circle(radius=self.star_radius, color=colours[i], fill_opacity=1).shift(
+                [star_df['x'].iloc[0], star_df['y'].iloc[0], 0]
+            )
+            star_objs[star_name] = star
+
+        for star_obj in star_objs.values():
+            self.add(star_obj)
+
+        # self.play(*[Write(star_obj) for star_obj in star_objs.values()])
+        self.wait(0.25)
+
+        for i in range(num_samples):
+            new_star_objs = {}
+            new_star_trace_objs = {}
+            transformations = []
+            for star_name in star_names:
+                # current star object
+                star = star_objs[star_name]
+
+                # trace
+                max_trace_length = 10
+                start_idx = max(0, i - max_trace_length)
+                if star_name not in star_trace_objs.keys():
+                    star_trace_objs[star_name] = self.create_trace(
+                        star,
+                        star_df_dict[star_name]['x'].iloc[start_idx:i],
+                        star_df_dict[star_name]['y'].iloc[start_idx:i],
+                    )
+                    self.add(star_trace_objs[star_name])
+                else:
+                    new_trace = self.create_trace(
+                        star,
+                        star_df_dict[star_name]['x'].iloc[start_idx:i],
+                        star_df_dict[star_name]['y'].iloc[start_idx:i],
+                    )
+                    transformations.append(
+                        ReplacementTransform(star_trace_objs[star_name], new_trace)
+                    )
+
+                # update position
+                newx = star_df_dict[star_name]['x'].iloc[i]
+                newy = star_df_dict[star_name]['y'].iloc[i]
+                moved_star = self.move_star(star, newx, newy)
+
+                # prepare for next iteration
+                new_star_objs[star_name] = moved_star
+                transformations.append(ReplacementTransform(star, moved_star))
+
+            self.play(*transformations, run_time=move_run_time)
+            star_objs = new_star_objs
+            star_trace_objs = new_star_trace_objs
+
+    def move_star(self, star_obj, x, y):
+        return star_obj.copy().move_to([x, y, 0])
+
+    def create_trace(self, star_obj, xs, ys):
+        color = star_obj.get_color()
+        xs = np.array(xs)
+        ys = np.array(ys)
+        dots = [Dot([xs[i], ys[i], 0], color=color, radius=0.005) for i in range(len(xs))]
+        arcs = [
+            Line(color=color, stroke_width=0.2, stroke_opacity=0.25).put_start_and_end_on(
+                dots[i].get_center(), dots[i + 1].get_center()
+            )
+            for i in range(len(dots) - 1)
+        ]
+        return VGroup(*arcs)
